047e365ed034b936bfc2bd6f178b40ad99a52087
Alexei Kubarev <aleksei.kubarev@aisa.ru>
2022-03-20 13:58:14 +0300

    Version 1.0.0

--- demoSpringBatch/src/main/java/com/example/demoSpringBatch/repository/EventRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.demoSpringBatch.repository;
-
-import com.example.demoSpringBatch.entity.EventObject;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface EventRepository extends JpaRepository<EventObject, Long> {
-}
